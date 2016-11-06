2d614cd31c52c4dd6314b174c86c60c2f9f2bbb8
alice-rdz <blanka_ni@hotmail.com>
2016-11-05 20:03:38 -0600

    Seed suelos

--- api/models/Suelos.js
+++ api/models/Suelos.js
@@ -4,11 +4,20 @@
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
+var types_ground = ['arcillosos','limosos','arenosos','margosos','gredosos','pantanosos']
 
 module.exports = {
 
   attributes: {
   	nombre: 'string'
+  },
+  seedData: [],
+  fillData: function() {
+    for (i = 0; i < types_ground.length; i++) {
+      	this.seedData.push({
+        	nombre: types_ground[i],
+      	});
+    }
   }
 };
 
